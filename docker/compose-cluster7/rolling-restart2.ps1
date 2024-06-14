ee0951f1e7288bf868b15c01c2852a60a19c9501
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-06-14 13:50:26 +0200

    cluster-5

--- docker/compose-cluster7/rolling-restart2.ps1
+++ docker/compose-cluster7/rolling-restart2.ps1
@@ -8,16 +8,7 @@ for($i=1; $i -le 7; $i++)
 	Write-Host (Get-Date).ToString() " Stopping container: $containerName"
 	# Loop to continuously execute the command, wait for the container to stop, and then start it again
 	# Run the command inside the container
-	# docker exec -it $containerName kill 1
-	docker stop $containerName
-
-	# Wait for the container to stop
-	Write-Host (Get-Date).ToString() " Waiting for container to stop..."
-	
-	do {
-		$containerStatus = docker inspect --format '{{.State.Status}}' $containerName
-		Start-Sleep -Seconds 1
-	} while ($containerStatus -eq "running") 
+	docker exec -it $containerName kill 1
 
 	Write-Host (Get-Date).ToString() " Container has stopped."
 
@@ -25,13 +16,7 @@ for($i=1; $i -le 7; $i++)
 	Write-Host (Get-Date).ToString() " Starting container: $containerName"
 	docker start $containerName
 
-	Write-Host (Get-Date).ToString() " Container started. Waiting for cluster to catch-up"
-	do {
-		$underReplicatedPartitions = docker exec -it $containerName /opt/kafka/bin/kafka-topics.sh --describe --under-replicated-partitions --bootstrap-server $bootstrapServer
-		Start-Sleep -Seconds 1
-	} while ($underReplicatedPartitions) 
-
-	Write-Host (Get-Date).ToString() " Waiting 30s for cluster to rebalance"
+	Write-Host (Get-Date).ToString() " Waiting 5s"
 	# Wait for cluster to rebalance
-	Start-Sleep -Seconds 30
+	Start-Sleep -Seconds 5
 }}
