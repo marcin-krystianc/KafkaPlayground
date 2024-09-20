eb246058488c0f61a6a3b1cac056445d1ee72383
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-09-20 09:09:13 +0200

    asan

--- dotnet/dev/librdkafka.sh
+++ dotnet/dev/librdkafka.sh
@@ -2,6 +2,7 @@
 set -eu
 
 dotnet build ../KafkaTool.csproj
-(cd ../../../librdkafka && ./dev-conf.sh)
+# (cd ../../../librdkafka && ./dev-conf.sh)
+(cd ../../../librdkafka && ./dev-conf.sh asan)
 cp ../../../librdkafka/src/librdkafka.so ../bin/Debug/net8.0/runtimes/linux-x64/native/
 cp ../../../librdkafka/src/librdkafka.so ../bin/Release/net8.0/runtimes/linux-x64/native/
