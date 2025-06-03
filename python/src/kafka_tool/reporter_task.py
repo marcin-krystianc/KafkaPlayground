a8cc8ed2f75e84055fbd02403081b902d03069d3
Marcin Krystianc <marcin.krystianc@gmail.com>
2025-06-03 14:05:53 +0200

    poll_thread for admin client

--- python/src/kafka_tool/reporter_task.py
+++ python/src/kafka_tool/reporter_task.py
@@ -21,23 +21,35 @@ def run_reporter_task(
 
     log.info("Running reporter task")
     admin_client = get_admin_client(config);
-    admin_client.poll(0.1);
+    # Start background polling thread to make sure the OIDC token is refreshed on time
+    poll_thread = threading.Thread(
+        target=lambda: [
+            admin_client.poll(1)
+            for _ in iter(lambda: not shutdown.is_set(), False)
+        ],
+        daemon=True,
+    )
+    poll_thread.start()
 
-    while True:
-        for _ in range(10):
-            if not shutdown.is_set():
-                time.sleep(settings.reporting_cycle / 1000.0 / 10)
-            else:
-                return
-        consumed, produced, duplicated, out_of_order = data.get_stats()
-        newly_produced = produced - prev_produced
-        newly_consumed = consumed - prev_consumed
-        prev_produced = produced
-        prev_consumed = consumed
-        topics_count = len(admin_client.list_topics(timeout=30).topics)
+    try:
+        while True:
+            for _ in range(10):
+                if not shutdown.is_set():
+                    time.sleep(settings.reporting_cycle / 1000.0 / 10)
+                else:
+                    return
+            consumed, produced, duplicated, out_of_order = data.get_stats()
+            newly_produced = produced - prev_produced
+            newly_consumed = consumed - prev_consumed
+            prev_produced = produced
+            prev_consumed = consumed
+            topics_count = len(admin_client.list_topics(timeout=30).topics)
 
-        elapsed_s = int(time.monotonic() - start)
-        log.info(
-            "Elapsed: %d s, %d (+%d) messages produced, %d (+%d) messages consumed, %d duplicated, %d out of sequence, topics_count=%d",
-            elapsed_s, produced, newly_produced, consumed, newly_consumed, duplicated, out_of_order, topics_count
-        )
+            elapsed_s = int(time.monotonic() - start)
+            log.info(
+                "Elapsed: %d s, %d (+%d) messages produced, %d (+%d) messages consumed, %d duplicated, %d out of sequence, topics_count=%d",
+                elapsed_s, produced, newly_produced, consumed, newly_consumed, duplicated, out_of_order, topics_count
+            )
+
+    finally:
+        poll_thread.join()  # Wait up to 1 second for clean shutdown
