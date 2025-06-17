611f5643b0279020b97075e1e8d6696b9bfc943a
Marcin Krystianc <marcin.krystianc@gmail.com>
2025-06-17 14:15:11 +0200

    ConsumerWithCallbacksTask

--- dotnet/KafkaTool/ConsumerWithCallbacksTask.cs
+++ dotnet/KafkaTool/ConsumerWithCallbacksTask.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿// https://github.com/marcin-krystianc/confluent-kafka-dotnet/commits/dev-20250609-alloc_free/
+#if CONSUME_WITH_CALLBACK
+
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Confluent.Kafka;
 using Microsoft.Extensions.Configuration;
@@ -9,15 +13,18 @@ using Microsoft.Extensions.Logging;
 
 namespace KafkaTool;
 
-class MyInt64Deserializer : IDeserializer<long>
+public static class ConsumerWthCallbacksTask
 {
-    public long Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
-    {
-        if (isNull)
+    private static readonly ILogger Log = LoggerFactory
+        .Create(builder => builder.AddSimpleConsole(options =>
         {
-            throw new ArgumentNullException($"Null data encountered deserializing Int64 value.");
-        }
-
+            options.SingleLine = true;
+            options.TimestampFormat = "HH:mm:ss ";
+        }))
+        .CreateLogger("Log");
+    
+    static long DeserializeInt64(ReadOnlySpan<byte> data)
+    {
         // network byte order -> big endian -> most significant byte in the smallest address.
         long result = ((long)data[0]) << 56 |
                       ((long)(data[1])) << 48 |
@@ -28,18 +35,17 @@ class MyInt64Deserializer : IDeserializer<long>
                       ((long)(data[6])) << 8 |
                       (data[7]);
         return result;
+    }    
+    
+    static long DeserializeInt32(ReadOnlySpan<byte> data)
+    {
+        // network byte order -> big endian -> most significant byte in the smallest address.
+        long result = ((long)(data[0])) << 24 |
+                      ((long)(data[1])) << 16 |
+                      ((long)(data[2])) << 8 |
+                      (data[3]);
+        return result;
     }
-}
-
-public static class ConsumerTask
-{
-    private static readonly ILogger Log = LoggerFactory
-        .Create(builder => builder.AddSimpleConsole(options =>
-        {
-            options.SingleLine = true;
-            options.TimestampFormat = "HH:mm:ss ";
-        }))
-        .CreateLogger("Log");
 
     public static Task GetTask(ProducerConsumerSettings settings, ProducerConsumerData data)
     {
@@ -55,7 +61,7 @@ public static class ConsumerTask
                 }))
                 .CreateLogger($"Consumer:");
 
-            logger.Log(LogLevel.Information, "Starting consumer task:");
+            logger.Log(LogLevel.Information, "Starting consumer with callbacks task:");
 
             var config = new ConsumerConfig
             {
@@ -114,8 +120,7 @@ public static class ConsumerTask
                 .SetPartitionsRevokedHandler((_, l) => logger.Log(LogLevel.Information,
                     $"Consumer log: PartitionsRevokedHandler: count={l.Count}"))
                 .SetPartitionsLostHandler((_, l) => logger.Log(LogLevel.Information,
-                    $"Consumer log: PartitionsLostHandler: count={l.Count}"))
-                .SetValueDeserializer(new MyInt64Deserializer());
+                    $"Consumer log: PartitionsLostHandler: count={l.Count}"));
             
             if (settings.SetOAuthTokenCallback)
             {
@@ -131,51 +136,54 @@ public static class ConsumerTask
 
                 consumer.Subscribe(topics);
 
-                Dictionary<(string Topic, int Key), ConsumeResult<int, long>> valueDictionary = new();
+                Dictionary<(string Topic, int Key), long> valueDictionary = new();
+                
+                ConsumeCallback callback = (in MessageReader mr) =>
+                {
+                    data.IncrementConsumed();
+                    if (mr.IsPartitionEOF)
+                    {
+                        throw new Exception($"Reached end of topic {mr.Topic}, partition {mr.Partition}, offset {mr.Offset}.");
+                    }
+                    
+                    if (settings.EnableSequenceValidation)
+                    {
+                        var latency = DateTime.UtcNow - mr.Timestamp.UtcDateTime;
+                        data.DigestConsumerLatency(latency.TotalMilliseconds);
 
+                        var key = (int)DeserializeInt32(mr.KeySpan);
+                        var dictKey = (mr.Topic, key);
+                        var msgValue = DeserializeInt64(mr.ValueSpan);
+                        // var key = (mr.Topic,  Encoding.UTF8.GetString());
+                        var offset = mr.Offset;
+                        offsetDictionary.AddOrUpdate(new TopicPartition(mr.Topic, mr.Partition), offset, (_, _) => offset);
+
+                        if (valueDictionary.TryGetValue(dictKey, out var previousConsumeResult))
+                        {
+                            if (msgValue != previousConsumeResult + 1)
+                            {
+                                logger.Log(LogLevel.Error,
+                                    $"Unexpected message value, topic/k [p]={mr.Topic}/{key} {mr.Partition}, Offset=?/{offset}, " +
+                                    $"LeaderEpoch=?/{mr.LeaderEpoch},  previous value={previousConsumeResult}, messageValue={msgValue}!");
+
+                                if (msgValue < msgValue + 1)
+                                    data.IncrementDuplicated();
+
+                                if (msgValue > msgValue + 1)
+                                    data.IncrementOutOfOrder();
+                            }
+                        }
+
+                        valueDictionary[dictKey] = msgValue;
+                    }
+                };
+                
                 while (true)
                 {
                     try
                     {
-                        var consumeResult = consumer.Consume();
-                        data.IncrementConsumed();
+                        consumer.ConsumeWithCallback(1, callback);
 
-                        if (consumeResult.IsPartitionEOF)
-                        {
-                            throw new Exception(
-                                $"Reached end of topic {consumeResult.Topic}, partition {consumeResult.Partition}, offset {consumeResult.Offset}.");
-                        }
-
-                        if (settings.EnableSequenceValidation)
-                        {
-                            var latency = DateTime.UtcNow - consumeResult.Message.Timestamp.UtcDateTime;
-                            data.DigestConsumerLatency(latency.TotalMilliseconds);
-
-                            var key = (consumeResult.Topic, consumeResult.Message.Key);
-                            offsetDictionary.AddOrUpdate(consumeResult.TopicPartition, consumeResult.Offset, (_, _) => consumeResult.Offset);
-
-                            if (valueDictionary.TryGetValue(key, out var previousConsumeResult))
-                            {
-                                if (consumeResult.Message.Value != previousConsumeResult.Message.Value + 1)
-                                {
-                                    logger.Log(LogLevel.Error,
-                                        $"Unexpected message value, topic/k [p]={consumeResult.Topic}/{consumeResult.Message.Key} {consumeResult.Partition}, Offset={previousConsumeResult.Offset}/{consumeResult.Offset}, " +
-                                        $"LeaderEpoch={previousConsumeResult.LeaderEpoch}/{consumeResult.LeaderEpoch},  previous value={previousConsumeResult.Message.Value}, messageValue={consumeResult.Message.Value}!");
-
-                                    if (consumeResult.Message.Value < previousConsumeResult.Message.Value + 1)
-                                        data.IncrementDuplicated();
-
-                                    if (consumeResult.Message.Value > previousConsumeResult.Message.Value + 1)
-                                        data.IncrementOutOfOrder();
-                                }
-
-                                valueDictionary[key] = consumeResult;
-                            }
-                            else
-                            {
-                                valueDictionary[key] = consumeResult;
-                            }
-                        }
                     }
                     catch (ConsumeException e)
                     {
@@ -186,3 +194,5 @@ public static class ConsumerTask
         });
     }
 }
+
+#endif
