4c523e87aff8daea1bd579ec4555d58950c3803b
Marcin Krystianc <marcin.krystianc@gmail.com>
2025-08-04 10:22:18 +0200

    python only

--- dotnet/KafkaTool/dev/librdkafka.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-dotnet build ../KafkaTool.csproj
-#(cd ../../../../librdkafka && ./dev-conf.sh)
-(cd ../../../../librdkafka && ./dev-conf.sh asan)
-#(cd ../../../../librdkafka && ./dev-conf.sh clean) # Non-development clean build
-cp ../../../../librdkafka/src/librdkafka.so ../bin/Debug/net8.0/runtimes/linux-x64/native/
-cp ../../../../librdkafka/src/librdkafka.so ../bin/Release/net8.0/runtimes/linux-x64/native/
